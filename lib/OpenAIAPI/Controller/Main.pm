9706e6af1019906519cc2061c62db1ebd5a0cdbd
Gene Boggs <gene@ology.net>
2022-12-20 19:25:14 -0800

    WIP: Refactoring

--- lib/OpenAIAPI/Controller/Main.pm
+++ lib/OpenAIAPI/Controller/Main.pm
@@ -2,28 +2,23 @@ package OpenAIAPI::Controller::Main;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
 sub index ($self) {
-  my $thing = $self->param('thing') || '';
-  my $stuff = $self->every_param('stuff');
-  $stuff = [qw(abc 123 xyz 667)] unless @$stuff;
+  my $prompt = $self->param('prompt') || '';
   $self->render(
-    thing => $thing,
-    stuff => $stuff,
+    prompt => $prompt,
   );
 }
 
 sub update ($self) {
   my $v = $self->validation;
-  $v->required('thing')->size(0, 10);
-  $v->required('thing', 'trim');
-  my $thing = $v->param('thing');
-  if ($v->error('thing')) {
-    $self->flash(error => 'Invalid thing!');
-    $thing = '';
+  $v->required('prompt')->size(0, 10);
+  $v->required('prompt', 'trim');
+  my $prompt = $v->param('prompt');
+  if ($v->error('prompt')) {
+    $self->flash(error => 'Invalid prompt!');
+    $prompt = '';
   }
-  $v->optional('stuff', 'trim');
-  my $stuff = $v->every_param('stuff');
   $self->redirect_to(
-    $self->url_for('index')->query(thing => $thing, stuff => $stuff)
+    $self->url_for('index')->query(prompt => $prompt)
   );
 }
 
