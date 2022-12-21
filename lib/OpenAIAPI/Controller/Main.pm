82682c7998a9472b19d6656b69cc322f71a05007
Gene Boggs <gene@ology.net>
2022-12-20 19:31:55 -0800

    Add OpenAI::API

--- lib/OpenAIAPI/Controller/Main.pm
+++ lib/OpenAIAPI/Controller/Main.pm
@@ -1,10 +1,14 @@
 package OpenAIAPI::Controller::Main;
 use Mojo::Base 'Mojolicious::Controller', -signatures;
 
+use OpenAI::API ();
+
 sub index ($self) {
   my $prompt = $self->param('prompt') || '';
+  my $responses = $self->every_param('responses');
   $self->render(
-    prompt => $prompt,
+    prompt    => $prompt,
+    responses => $responses,
   );
 }
 
@@ -17,8 +21,24 @@ sub update ($self) {
     $self->flash(error => 'Invalid prompt!');
     $prompt = '';
   }
+  else {
+    my $openai = OpenAI::API->new(api_key => $self->config('api-key'));
+    my $response = $openai->completions(
+        model             => 'text-davinci-003',
+        prompt            => $prompt,
+        max_tokens        => 2048,
+        temperature       => 0.5,
+        top_p             => 1,
+        frequency_penalty => 0,
+        presence_penalty  => 0
+    );
+    my @responses;
+    for my $choice ($response->{choices}->@*) {
+        push @responses, $choice->{text};
+    }
+  }
   $self->redirect_to(
-    $self->url_for('index')->query(prompt => $prompt)
+    $self->url_for('index')->query(prompt => $prompt, responses => \@responses)
   );
 }
 
