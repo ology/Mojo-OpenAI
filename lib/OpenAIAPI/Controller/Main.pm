43d960636da279db5333acfbd6fa0b5ef25eb8fc
Gene Boggs <gene@ology.net>
2022-12-20 20:57:29 -0800

    Model is broken?

--- lib/OpenAIAPI/Controller/Main.pm
+++ lib/OpenAIAPI/Controller/Main.pm
@@ -5,7 +5,6 @@ use OpenAI::API ();
 
 sub index ($self) {
   my $prompt = $self->param('prompt') || '';
-  my $model = $self->param('model') || 'text-davinci-003';
   my $responses = $self->every_param('responses');
   $self->render(
     prompt    => $prompt,
@@ -16,19 +15,15 @@ sub index ($self) {
 sub update ($self) {
   my $v = $self->validation;
   $v->required('prompt');
-  $v->required('model');
   my $prompt = $v->param('prompt');
-  my $model = $v->param('model');
   my @responses;
-  if ($v->error('prompt') || $v->error('model')) {
+  if ($v->error('prompt')) {
     $self->flash(error => 'Invalid submission!');
     $prompt = '';
-    $model = '';
   }
   else {
     my $openai = OpenAI::API->new(api_key => $self->config('api-key'));
     my $response = $openai->completions(
-        model             => $model,
         prompt            => $prompt,
         max_tokens        => 2048,
         temperature       => 0.5,
