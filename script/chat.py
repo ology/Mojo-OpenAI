c152cee1bc09b85d5d87882a2c866973b64a7692
Gene Boggs <gene@ology.net>
2024-10-15 14:07:16 -0700

    Initial

---
+++ script/chat.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+import os
+import sys
+
+def get_response(messages):
+    client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+    response = client.chat.completions.create(
+        messages=messages,
+        model="gpt-4o-mini",
+    )
+    content = response.choices[0].message.content
+    return content
+
+def main():
+    if sys.argv[1]:
+        messages = sys.argv[1:]
+        response = get_response(messages)
+        print(response)
+
+if __name__ == "__main__":
+    main()
